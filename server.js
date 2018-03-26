7f5f0186e70bc0a627bf4e8ad2102192e0ee82ee
Dakota Edwards <drakoden2000@gmail.com>
2018-03-26 10:03:03 -0700

    server stuff

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-// Requiring our models for syncing
 var db = require("./models");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(express.static("public"));
-
-require("./routes/api-routes.js")(app);
+var PORT = process.env.PORT || 3000;
+var app = express();
 
 db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+    app.listen(PORT, function() {
+        console.log("Listening on port: ", PORT);
+    });
 });
