f556846c202f4d15dbf66d446b42b0d72e34c614
Dakota Edwards <drakoden2000@gmail.com>
2018-04-02 09:59:55 -0700

    hope this works

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var Nightmare = require("nightmare");
 var nightmare = Nightmare({ show: true });
 
 var page = nightmare
-  .goto("https://sleepy-cove-80178.herokuapp.com")
+  .goto("https://the-intoxicated-cactuar.herokuapp.com")
   .type("#search", "Tonberry")
   .click("#searchButton")
   .wait("#links a")
